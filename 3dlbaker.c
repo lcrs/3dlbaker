56e14c05b2cac508a8a52ccb6a9e021e8b6a567a
Lewis Saunders <lewis.saunders@gmail.com>
2013-09-25 00:02:36 +0100

    Writes header and footer, breaking for sausages

--- 3dlbaker.c
+++ 3dlbaker.c
@@ -46,8 +46,20 @@ int getbuf(int n, SparkMemBufStruct *b) {
 // Write the LUT
 unsigned long *bam(int i, SparkInfoStruct si) {
 	int fd;
+	const char *header = "#Tokens required by applications - do not edit\n"
+						 "3DMESH\n"
+						 "Mesh 4 12\n"
+						 "0 64 128 192 256 320 384 448 512 576 640 704 768 832 896 960 1023\n"
+						 "\n";
+	const char *footer = "#Tokens required by applications - do not edit\n"
+						 "\n"
+						 "LUT8\n"
+						 "gamma 1.0\n"
+						 "\n";
+
 	fd = open(SparkString16.Value, O_CREAT | O_WRONLY | O_TRUNC, 0000666);
-	write(fd, "hi", 2);
+	write(fd, header, strlen(header));
+	write(fd, footer, strlen(footer));
 	close(fd);
 	return(NULL);
 }
@@ -59,7 +71,22 @@ unsigned long *SparkProcess(SparkInfoStruct si) {
 	if(!getbuf(1, &result)) return(NULL);
 	if(!getbuf(2, &input)) return(NULL);
 
-	sparkCopyBuffer(input.Buffer, result.Buffer);
+	if(SparkBoolean14.Value) {
+		float i = 0.0;
+		for(int x = 0; x < result.BufWidth; x++) {
+			for(int y = 0; y < result.BufHeight; y++) {
+				half *r = (half *) (result.Buffer + y * result.Stride + x * result.Inc);
+				half *g = (half *) (result.Buffer + y * result.Stride + x * result.Inc) + sizeof(half);
+				half *b = (half *) (result.Buffer + y * result.Stride + x * result.Inc) + 2 * sizeof(half);
+				*r = i / (1920.0 * 1080.0);
+				*g = i / (1920.0 * 1080.0);
+				*b = i / (1920.0 * 1080.0);
+				i = i + 1.0;
+			}
+		}
+	} else {
+		sparkCopyBuffer(input.Buffer, result.Buffer);
+	}
 	return(result.Buffer);
 }
 
