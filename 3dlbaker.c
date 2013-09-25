6875d033e72f9bee7048cf0d66caa4d9d7c6a24a
Lewis Saunders <lewis.saunders@gmail.com>
2013-09-25 02:32:30 +0100

    Pulled finger out,  made it work

--- 3dlbaker.c
+++ 3dlbaker.c
@@ -10,8 +10,8 @@
 	#include "/usr/discreet/flame_2013.0.2/sparks/spark.h"
 #endif
 
-unsigned long *bam(int i, SparkInfoStruct si);
-unsigned long *toggle(int i, SparkInfoStruct si);
+unsigned long *bam(int what, SparkInfoStruct si);
+unsigned long *toggle(int what, SparkInfoStruct si);
 
 SparkBooleanStruct SparkBoolean14 = {
 	0,
@@ -45,28 +45,56 @@ int getbuf(int n, SparkMemBufStruct *b) {
 }
 
 // Write the LUT
-unsigned long *bam(int i, SparkInfoStruct si) {
-	int fd;
+unsigned long *bam(int what, SparkInfoStruct si) {
+	FILE *fd;
 	const char *header = "#Tokens required by applications - do not edit\n"
+						 "\n"
 						 "3DMESH\n"
 						 "Mesh 4 12\n"
 						 "0 64 128 192 256 320 384 448 512 576 640 704 768 832 896 960 1023\n"
 						 "\n";
-	const char *footer = "#Tokens required by applications - do not edit\n"
+	const char *footer = "\n" 
+						 "#Tokens required by applications - do not edit\n"
 						 "\n"
 						 "LUT8\n"
 						 "gamma 1.0\n"
 						 "\n";
 
-	fd = open(SparkString16.Value, O_CREAT | O_WRONLY | O_TRUNC, 0000666);
-	write(fd, header, strlen(header));
-	write(fd, footer, strlen(footer));
-	close(fd);
+	fd = fopen(SparkString16.Value, "w");
+	fprintf(fd, "%s", header);
+
+	SparkMemBufStruct input;
+	if(!getbuf(2, &input)) return(NULL);
+
+	char *p;
+	long i = 0;
+	for(long y = 0; y < input.BufHeight; y++) {
+		for(long x = 0; x < input.BufWidth; x++) {
+			p = ((char *) input.Buffer) + y * input.Stride + x * input.Inc;
+
+			unsigned short *r, *g, *b;
+			r = (unsigned short *) p;
+			g = (unsigned short *) (p + sizeof(short));
+			b = (unsigned short *) (p + 2 * sizeof(short));
+
+			fprintf(fd, "%d %d %d\n", *r / 16, *g / 16, *b / 16);
+
+			i++;
+
+			if(i >= (17 * 17 * 17)) {
+				goto phew;
+			}
+		}
+	}
+
+	phew:
+	fprintf(fd, "%s", footer);
+	fclose(fd);
 	return(NULL);
 }
 
 // Switch display
-unsigned long *toggle(int i, SparkInfoStruct si) {
+unsigned long *toggle(int what, SparkInfoStruct si) {
 	sparkReprocess();
 	return(NULL);
 }
@@ -79,20 +107,21 @@ unsigned long *SparkProcess(SparkInfoStruct si) {
 	if(!getbuf(2, &input)) return(NULL);
 
 	if(SparkBoolean14.Value) {
-		unsigned long i = 0;
 		char *p;
-		for(int y = 0; y < result.BufHeight; y++) {
-			for(int x = 0; x < result.BufWidth; x++) {
+		unsigned int steps[17] = {0, 64, 128, 192, 256, 320, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1023};
+		long i = 0;
+		for(long y = 0; y < result.BufHeight; y++) {
+			for(long x = 0; x < result.BufWidth; x++) {
 				p = ((char *) result.Buffer) + y * result.Stride + x * result.Inc;
 
-				unsigned int *r, *g, *b;
-				r = (unsigned int *) p;
-				g = (unsigned int *) (p + sizeof(short));
-				b = (unsigned int *) (p + 2 * sizeof(short));
-				
-				*r = x << 4;
-				*g = y << 4;
-				*b = 4095 << 4;
+				unsigned short *r, *g, *b;
+				r = (unsigned short *) p;
+				g = (unsigned short *) (p + sizeof(short));
+				b = (unsigned short *) (p + 2 * sizeof(short));
+
+				*r = steps[(i / (17 * 17)) % 17] * 64;
+				*g = steps[(i / (17 *  1)) % 17] * 64;
+				*b = steps[(i / ( 1 *  1)) % 17] * 64;
 
 				i++;
 			}
